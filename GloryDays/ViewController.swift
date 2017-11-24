3cf3ce2d04a30ea21a183fdd481185d995016771
Andres <andres@Andress-iMac.local>
2017-11-23 23:49:27 -0500

    primer commit

--- GloryDays/ViewController.swift
+++ GloryDays/ViewController.swift
@@ -7,8 +7,13 @@
 //
 
 import UIKit
+import AVFoundation
+import Photos
+import Speech
 
 class ViewController: UIViewController {
+
+    @IBOutlet var infoLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +24,48 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    @IBAction func askPermissions(_ sender: Any) {
+        self.requestPhotoPermission()
+        self.requestRecordPermission()
+        self.requestSpeechRecognizerPermission()
+    }
+    
+    func requestPhotoPermission(){
+        PHPhotoLibrary.requestAuthorization { [unowned self](AuthorizationStatus) in
+            DispatchQueue.main.async {
+                if(AuthorizationStatus == .authorized){
+                }
+                else {
+                    self.infoLabel.text = "No se ha concedido el permiso de fotos"
+                }
+            }
+        }
+    }
+    func requestRecordPermission(){
+        AVAudioSession.sharedInstance().requestRecordPermission { [unowned self](allowed) in
+            DispatchQueue.main.async {
+                if allowed{
+                }
+                else{
+                    self.infoLabel.text = "No se ha concedido el permiso de grabación de voz"
+                }
+            }
 
-
+        }
+    }
+    func requestSpeechRecognizerPermission(){
+        SFSpeechRecognizer.requestAuthorization { [unowned self](AuthorizationStatus) in
+            DispatchQueue.main.async {
+                if(AuthorizationStatus == .authorized){
+                    
+                }else{
+                    self.infoLabel.text = "No se ha concedido el permiso de transcripciòn de texto"
+                }
+            }
+        }
+    }
+    func authorizationCompleted(){
+        dismiss(animated: true)
+    }
 }
 
---
+++ GloryDays/imgCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  imgCollectionViewCell.swift
+//  GloryDays
+//
+//  Created by Andres on 23/11/17.
+//  Copyright © 2017 Andres. All rights reserved.
+//
+
+import UIKit
+
+class imgCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet var imageView: UIImageView!
+}
